b284353ae7b0d0b6ebac89bbe9231394f28b49d1
Rohit Mondal <rohit.luiji3@gmail.com>
2025-08-23 05:29:31 +0000

    triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,71 @@
 #include <iostream> 
 #include <globals.h>
-
+#include <vector>
 
 void GetOpenGLVerisionInfo(){
 	std::cout<< "Vendor: " << glGetString(GL_VENDOR) << std::endl;
 	std::cout<< "Renderer: " << glGetString(GL_RENDERER) << std::endl;
 	std::cout<< "Shading Language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+}
+
+void VertexSpecification(){
+	//on cpu
+	const std::vector<GLfloat> vertexPosition{
+		-0.8f,-0.8f,0.0f,
+		 0.8f,-0.8f,0.0f,
+		 0.0f,0.8f,0.0f,
+	};
+	//gpu setup
+	glGenVertexArrays(1,&Globals::gVertexArrayObj);
+	glBindVertexArray(Globals::gVertexArrayObj);
+	//vbo
+	glGenBuffers(1,&Globals::gVertexBufferObject);
+	glBindBuffer(GL_ARRAY_BUFFER,Globals::gVertexBufferObject);
+	glBufferData(GL_ARRAY_BUFFER,
+		vertexPosition.size()*sizeof(GLfloat),
+		vertexPosition.data(),
+		GL_STATIC_DRAW	
+	);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0,
+		3,
+		GL_FLOAT,
+		GL_FALSE,
+		0,
+		(void*)0	
+	);
+	glBindVertexArray(0);
+	glDisableVertexAttribArray(0);
+}
+
+GLuint CompileShader(GLuint type, const std::string& source){
+	GLuint shaderObject;
+	if(type==GL_VERTEX_SHADER){
+		shaderObject = glCreateShader(GL_VERTEX_SHADER);
+	}else if (type == GL_FRAGMENT_SHADER){
+		shaderObject = glCreateShader(GL_FRAGMENT_SHADER);
+	}
+	const char* src = source.c_str();
+	glShaderSource(shaderObject,1,&src,nullptr);
+	glCompileShader(shaderObject);
+	return shaderObject;
+}
+
+GLuint CreateShaderProgram(const std::string& vertexShaderSource, const std::string& fragmentShaderSource){
+	GLuint programObject = glCreateProgram();
+
+	GLuint myVertexShader = CompileShader(GL_VERTEX_SHADER,vertexShaderSource);
+	GLuint myFragmentShader = CompileShader(GL_FRAGMENT_SHADER,fragmentShaderSource);
+	glAttachShader(programObject,myVertexShader);
+	glAttachShader(programObject,myFragmentShader);
+	glLinkProgram(programObject);
+	glValidateProgram(programObject);
+
+	return programObject;
+}
+
+void CreateGraphicsPipeline(){
+	Globals::gGraphicsPipelineShaderProgram = CreateShaderProgram(Globals::gVertexShaderSource,Globals::gFragmentShaderSource);
 }
 
 void init(){
@@ -51,8 +111,23 @@ void Input(){
 		}
 	}
 }
-void PreDraw(){}
-void Draw(){}
+void PreDraw(){
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_CULL_FACE);
+
+	glViewport(0,0,Globals::gScreenWidth,Globals::gScreenHeight);
+	glClearColor(1.f,1.f,0.f,1.f);
+
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(Globals::gGraphicsPipelineShaderProgram);
+
+}
+void Draw(){
+	glBindVertexArray(Globals::gVertexArrayObj);
+	glBindBuffer(GL_ARRAY_BUFFER,Globals::gVertexBufferObject);
+	glDrawArrays(GL_TRIANGLES,0,3);
+}
 
 void loop(){
 
@@ -73,6 +148,11 @@ void CleanUp(){
 
 int main(){
 	init();
+
+	//vao
+	VertexSpecification();
+	CreateGraphicsPipeline();
+
 	loop();
 	CleanUp();
 	return 0;
